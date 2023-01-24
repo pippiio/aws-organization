f189387eef90695912b2de8a7a4604b70f33c0a5
Joachim Rorbol <jr@efio.dk>
2023-01-24 11:18:03 +0100

    Owerwriting iam user ssm params

--- account.tf
+++ account.tf
@@ -28,21 +28,47 @@ resource "aws_iam_user" "iam_service_user" {
   tags = local.default_tags
 }
 
-resource "aws_ssm_parameter" "iam_service_user" {
-  for_each = { for key, account in local.accounts : key => account if account.create_iam_user }
+resource "aws_ssm_parameter" "iam_service_user_access_key_id" {
+  for_each = { for key, account in local.accounts : "/account/${aws_organizations_account.this[key].id}/aws_access_key_id" => {
+    account_id = aws_organizations_account.this[key].id
+    content    = aws_iam_access_key.iam_service_user[key].id
+  } if account.create_iam_user }
 
-  name        = "/accounts/${aws_organizations_account.this[each.key].id}/iam_user"
-  description = "Credentials for IAM user that can assume ${aws_organizations_account.this[each.key].id} organization admin role."
+  name        = each.key
+  description = format("AWS access key id to assume %s organization admin role", each.value.account_id)
+  type        = "String"
+  overwrite   = true
+  value       = each.value.content
+  tags        = local.default_tags
+}
+
+resource "aws_ssm_parameter" "iam_service_user_secret_access_key" {
+  for_each = { for key, account in local.accounts : "/account/${aws_organizations_account.this[key].id}/aws_secret_access_key" => {
+    account_id = aws_organizations_account.this[key].id
+    content    = aws_iam_access_key.iam_service_user[key].secret
+  } if account.create_iam_user }
+
+  name        = each.key
+  description = format("AWS access key secret to assume %s organization admin role", each.value.account_id)
   type        = "SecureString"
   key_id      = aws_kms_key.this.arn
-  value = jsonencode({
-    aws_access_key_id     = aws_iam_access_key.iam_service_user[each.key].id
-    aws_secret_access_key = aws_iam_access_key.iam_service_user[each.key].secret
-    aws_assume_role_arn   = format(local.organization_role_arn_template, aws_organizations_account.this[each.key].id)
-    aws_account_id        = aws_organizations_account.this[each.key].id
-  })
+  overwrite   = true
+  value       = each.value.content
+  tags        = local.default_tags
+}
 
-  tags = local.default_tags
+resource "aws_ssm_parameter" "iam_service_user_assume_role_arn" {
+  for_each = { for key, account in local.accounts : "/account/${aws_organizations_account.this[key].id}/aws_assume_role_arn" => {
+    account_id = aws_organizations_account.this[key].id
+    content    = format(local.organization_role_arn_template, aws_organizations_account.this[key].id)
+  } if account.create_iam_user }
+
+  name        = each.key
+  description = format("The AWS role to assume in account %s", each.value.account_id)
+  type        = "String"
+  overwrite   = true
+  value       = each.value.content
+  tags        = local.default_tags
 }
 
 resource "aws_iam_access_key" "iam_service_user" {
