2a9bd70f117854cdf094bf66c79ad53e8647c720
Jes Struck <mail@jesstruck.dk>
2025-09-30 10:53:08 +0200

    feat: Fix 'Error: Invalid for_each argument' (#30)
    * feat: Fix 'Error: Invalid for_each argument'
    
    * chore: bump upload-artifact to version v4

--- account.tf
+++ account.tf
@@ -33,42 +33,42 @@ resource "aws_iam_user" "iam_service_user" {
 }
 
 resource "aws_ssm_parameter" "iam_service_user_access_key_id" {
-  for_each = { for key, account in local.accounts : "/account/${aws_organizations_account.this[key].id}/aws_access_key_id" => {
-    account_id = aws_organizations_account.this[key].id
-    content    = aws_iam_access_key.iam_service_user[key].id
-  } if account.create_iam_user }
+  for_each = { for key, account in local.accounts :
+    key => account
+    if account.create_iam_user
+  }
 
-  name        = each.key
+  name        = "/account/${aws_organizations_account.this[each.key].id}/aws_access_key_id"
   description = format("AWS access key id to assume %s organization admin role", each.value.account_id)
   type        = "String"
-  value       = each.value.content
+  value       = aws_iam_access_key.iam_service_user[each.key].id
   tags        = local.default_tags
 }
 
 resource "aws_ssm_parameter" "iam_service_user_secret_access_key" {
-  for_each = { for key, account in local.accounts : "/account/${aws_organizations_account.this[key].id}/aws_secret_access_key" => {
-    account_id = aws_organizations_account.this[key].id
-    content    = aws_iam_access_key.iam_service_user[key].secret
-  } if account.create_iam_user }
+  for_each = { for key, account in local.accounts :
+    key => account
+    if account.create_iam_user
+  }
 
-  name        = each.key
-  description = format("AWS access key secret to assume %s organization admin role", each.value.account_id)
+  name        = "/account/${aws_organizations_account.this[each.key].id}/aws_secret_access_key"
+  description = format("AWS access key secret to assume %s organization admin role", aws_organizations_account.this[each.key].id)
   type        = "SecureString"
   key_id      = aws_kms_key.this.arn
-  value       = each.value.content
+  value       = aws_iam_access_key.iam_service_user[each.key].secret
   tags        = local.default_tags
 }
 
 resource "aws_ssm_parameter" "iam_service_user_assume_role_arn" {
-  for_each = { for key, account in local.accounts : "/account/${aws_organizations_account.this[key].id}/aws_assume_role_arn" => {
-    account_id = aws_organizations_account.this[key].id
-    content    = format(local.organization_role_arn_template, aws_organizations_account.this[key].id)
-  } if account.create_iam_user }
+  for_each = { for key, account in local.accounts :
+    key => account
+    if account.create_iam_user
+  }
 
-  name        = each.key
-  description = format("The AWS role to assume in account %s", each.value.account_id)
+  name        = "/account/${aws_organizations_account.this[each.key].id}/aws_assume_role_arn"
+  description = format("The AWS role to assume in account %s", aws_organizations_account.this[each.key].id)
   type        = "String"
-  value       = each.value.content
+  value       = format(local.organization_role_arn_template, aws_organizations_account.this[each.key].id)
   tags        = local.default_tags
 }
 
